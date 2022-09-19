68b5d267e1f3c7e24faa73ca984f7c4a894077c1
Qozeem Salami <qozeemopeyemi2008@gmail.com>
2022-09-19 11:56:40 +0100

    completed truthy and falsy values

--- Class Exercises/Truthy and falsy values/script.js
+++ Class Exercises/Truthy and falsy values/script.js
@@ -1,1 +1,19 @@
-// falsy values are values that are ot exactly false but will become false whenever we try to convert them
+// falsy values are values that are ot exactly false but will become false whenever we try to convert them into a boolean while reverse is the case for truthy values
+
+// 5 falsy values: 0, " ", undefined, null, NaN
+// everything else are truthy values
+
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+
+// EXAMPLE
+const money = 0;
+if (money) {
+  console.log("Save some 💵 ");
+} else {
+  console.log("You should et a job! 💪");
+}
+
+// money variable is converted into boolean i.e falsy value that mean the else block is executed
